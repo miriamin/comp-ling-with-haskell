bc7de96b098c4af67f29c77da46bb8ad697520dc
Miriam Amin <miriam_amin@web.de>
2019-03-27 20:32:53 +0100

    transducer for clustering. for #2

--- uppersaxon.hs
+++ uppersaxon.hs
@@ -5,6 +5,7 @@ module UpperSaxon where
 -- https://home.uni-leipzig.de/gkobele/courses/2018.WS/CompLing/index.html
 
 import Dfst
+import Data.Maybe
 -- import IPA
 --import IPA_Test
 --import Data.Maybe
@@ -21,6 +22,7 @@ sonorant s = vowel s || (s == 'm') || (s == 'n') || (s == 'ŋ') || (s == 'l') ||
 notSonorant = not . sonorant
 vowel s = (s == 'a') || (s == 'e') || (s == 'i') || (s == 'o') || (s == 'u')
 voicedObstruent s = (s == 'b') || (s == 'd') || (s == 'g') 
+germanFinal s = (s == 'g') || (s=='ç') || (s=='ʃ')
 
 unvoice :: Char -> Char
 unvoice obstruent
@@ -62,7 +64,8 @@ rmVoicing = mkForwardTransducer states ['a'..'z'] ['a'..'z'] Zero "" delta final
 
 -- alle Obstruenten werden stimmlos
 data UV_States = UV_Zero | UV_Phoneme
-data TL_States = TL_Zero | TL_SeenInitialK | TL_SeenNone | TL_SeenLafterInitialK
+data TL_States = TL_Zero | TL_SeenInitialK | TL_SeenNone | TL_SeenLafterInitialK 
+data C_States = C_Zero | C_SeenNone | C_SeenI | C_SeenFinal Char
 
 rmVoicing:: ForwardTrans UV_States Char String
 rmVoicing = mkForwardTransducer [UV_Zero, UV_Phoneme] ['a'..'z'] ['a'..'z'] UV_Zero "" delta final
@@ -89,7 +92,21 @@ tlize = mkForwardTransducer [TL_Zero, TL_SeenInitialK, TL_SeenNone, TL_SeenLafte
 -- * final -ig/-ich/-isch in standard german becomes a /ʃ/-cluster (p 234)
 -- since I could not make the BackwardTrans from dfst module working (recognizes function is not implemented for BackwardTrans?) 
 -- I used a little hack (just reversed the string before)
-                  
+-- so this machine works like a Forwards trans, which means it transforms strings that START with -gi/ etc.
+cluster::  ForwardTrans C_States Char String        
+cluster = mkForwardTransducer states ['a'..'z'] ['a'..'z'] C_Zero [] delta final
+  where
+   states = (C_Zero : C_SeenNone : C_SeenI :[C_SeenFinal c | c <- ['a'..'z'], germanFinal c])
+   final (C_SeenFinal b) = Just [b]
+   final _ = Just ""
+   delta C_Zero c | germanFinal c = Just (C_SeenFinal c, "")
+                  | otherwise = Just (C_SeenNone, [c])
+   delta C_SeenI c = Just (C_SeenNone, "ʃ" ++ [c])
+   delta C_SeenNone c = Just (C_SeenNone, [c])
+   delta (C_SeenFinal b) c | (==) 'i' c = Just(C_SeenI, "")
+                           | otherwise = Just (C_SeenNone, [b,c])
+
+                
                  
 -- testing: (transduce rmVoicing) input1
 input1 = "der hund bellt"
@@ -98,5 +115,5 @@ output1 = "pellt"
 -- testing: (transduce tlize) input2
 input2 = "klavierlk"
 
--- testing: 
+-- testing: reverse $ fromJust $ (transduce cluster) input3
 input3 = reverse "lustig"
